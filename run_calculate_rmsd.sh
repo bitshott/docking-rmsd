74d56844130d432eb02036dd6a6c81d1d40595f2
Matik01 <piupip4@gmail.com>
2025-07-14 16:15:14 +0300

    Fixed error

--- run_calculate_rmsd.sh
+++ run_calculate_rmsd.sh
@@ -3,12 +3,12 @@
 LIG_DIR="data_lig"
 SDF_DIR="sdf_filter"
 
-# for dir in *_package; do
-# 	receptor_id="${dir%_package}"
-# 	echo "Running: $receptor_id"
-# 	echo "${LIG_DIR}/${receptor_id}_ligand.pdb"
-# 	python3 calculate_docking_rmsd.py --workspace_dir "${dir}" --rec_id "$receptor_id" --ref_ligand "${LIG_DIR}/${receptor_id}_ligand.pdb" --output_csv "${receptor_id}.csv" --data_csv "data_csv"
-# done
+for dir in *_package; do
+	receptor_id="${dir%_package}"
+	echo "Running: $receptor_id"
+	echo "${LIG_DIR}/${receptor_id}_ligand.pdb"
+	python3 calculate_docking_rmsd.py --workspace_dir "${dir}" --rec_id "$receptor_id" --ref_ligand "${LIG_DIR}/${receptor_id}_ligand.pdb" --output_csv "${receptor_id}.csv" --data_csv "data_csv"
+done
 
 mkdir $SDF_DIR
 mv *_package.sdf $SDF_DIR
